739b76a96cb44b69c50c1f990074ab0f5812ecaa
rajeshedwar <rajeshedwar@gmail.com>
2019-06-17 16:28:17 +0530

    initial

---
+++ CatalogAPI/Controllers/CatalogController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using CatalogAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CatalogAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CatalogController : ControllerBase
+    {
+        private static IList<Product> products = new List<Product>()
+        {
+            new Product{Id=1,Name="Apple",Price=100,Quantity=20},
+            new Product{Id=2,Name="Orange",Price=150,Quantity=30}
+        };
+
+        [HttpGet("", Name = "GetAll")]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        public IEnumerable<Product> GetItems()
+        {
+            return products;
+        }
+
+        [HttpGet("{id}", Name ="GetById")]
+        [ProducesResponseType((int) HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        public ActionResult<Product> GetItemById([FromRoute] int id)
+        {
+            var item = products.Single(s => s.Id == id);
+            if (item == null)
+                return NotFound();
+            else
+                return item;
+        }
+        [HttpPost("", Name = "AddItem")]
+        [ProducesResponseType((int)HttpStatusCode.Created)]
+        public ActionResult<Product> AddItem([FromBody] Product product)
+        {
+            product.Id = products.Max(s => s.Id) + 1;
+            products.Add(product);
+            return Created("", product);
+        }
+    }
+}
---
+++ CatalogAPI/Startup.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace CatalogAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSwaggerGen(config =>
+            {
+                config.SwaggerDoc("v1", new Info
+                {
+                    Title = "Catalog API",
+                    Version = "1.0",
+                    Contact = new Contact { Name = "Rajesh", Email = "rajesh@html.com" },
+                    Description = "Operations for catalog management"
+                });
+            });
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "Catalog Management API");
+                options.RoutePrefix = "";
+            });
+
+            app.UseMvc();
+        }
+    }
+}
